f2e5d6ae77caf578a08c6d74a00897b4c690774a
Aleksey Bormatov <inferio3@gmail.com>
2025-08-17 22:19:34 +0300

    jhbjh

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -26,19 +26,22 @@ def get_opinion(id):
 @app.route('/api/opinions/<int:id>/', methods=['PATCH'])
 def update_opinion(id):
     data = request.get_json()
-    # Если метод get_or_404 не найдёт указанный ID,
-    # то он выбросит исключение 404:
+    if (
+        'text' in data and 
+        Opinion.query.filter_by(text=data['text']).first() is not None
+    ):
+        # При неуникальном значении поля text
+        # возвращаем сообщение об ошибке в формате JSON 
+        # и статус-код 400:
+        return jsonify({'error':
+                        'Такое мнение уже есть в базе данных'}), 400
     opinion = Opinion.query.get_or_404(id)
     opinion.title = data.get('title', opinion.title)
     opinion.text = data.get('text', opinion.text)
     opinion.source = data.get('source', opinion.source)
     opinion.added_by = data.get('added_by', opinion.added_by)
-    # Все изменения нужно сохранить в базе данных.
-    # Объект opinion добавлять в сессию не нужно.
-    # Этот объект получен из БД методом get_or_404() и уже хранится в сессии.
     db.session.commit()  
-    # При изменении объекта вернём сам объект и код 200:
-    return jsonify({'opinion': opinion.to_dict()}), 200
+    return jsonify({'opinion': opinion.to_dict()}), 201
 
 
 @app.route('/api/opinions/<int:id>/', methods=['DELETE'])
@@ -62,17 +65,22 @@ def get_opinions():
 
 @app.route('/api/opinions/', methods=['POST'])
 def add_opinion():
-    # Получение данных из запроса в виде словаря:
     data = request.get_json()
-    # Создание нового пустого экземпляра модели:
+    if 'title' not in data or 'text' not in data:
+        return jsonify({'error':
+                        'В запросе отсутствуют обязательные поля'}), 400
+    # Если в базе данных уже есть объект
+    # с таким же значением поля text...
+    if Opinion.query.filter_by(text=data['text']).first() is not None:
+        # ...возвращаем сообщение об ошибке в формате JSON
+        # и статус-код 400:
+        return jsonify({'error':
+                        'Такое мнение уже есть в базе данных'}), 400
     opinion = Opinion()
-    # Наполнение экземпляра данными из запроса:
     opinion.from_dict(data)
-    # Добавление новой записи в сессию:
     db.session.add(opinion)
-    # Сохранение изменений:
     db.session.commit()
-    return jsonify({'opinion': opinion.to_dict()}), 201
+    return jsonify({'opinion': opinion.to_dict()}), 201 
 
 
 @app.route('/api/get-random-opinion/', methods=['GET'])
