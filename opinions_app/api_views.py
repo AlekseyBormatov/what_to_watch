837fb06a108ff8bc43ff18b8c4950ed9f20d0b9d
Aleksey Bormatov <inferio3@gmail.com>
2025-08-17 22:00:27 +0300

    333

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -5,6 +5,13 @@ from flask import jsonify, request
 
 from . import app, db
 from .models import Opinion
+from .views import random_opinion
+
+
+@app.route('/api/get-random-opinion/', methods=['GET'])
+def get_random_opinion():
+    opinion = random_opinion()
+    return jsonify({'opinion': opinion.to_dict()}), 200
 
 
 # Явно разрешаем метод GET:
@@ -66,3 +73,10 @@ def add_opinion():
     # Сохранение изменений:
     db.session.commit()
     return jsonify({'opinion': opinion.to_dict()}), 201
+
+
+@app.route('/api/get-random-opinion/', methods=['GET'])
+def get_random_opinion():
+    opinion = random_opinion()
+    return jsonify({'opinion': opinion.to_dict()}), 200
+
